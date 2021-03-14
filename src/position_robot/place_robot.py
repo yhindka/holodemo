b5922f760bbf4e1c4542c7cbb5933312f7a6396c
yhindka <yhindka@gmail.com>
2021-03-14 01:20:42 +0000

    working on publishing correct position

--- src/position_robot/place_robot.py
+++ src/position_robot/place_robot.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python
 
 import rospy
-import tf
-from geometry_msgs.msg import Pose
+import tf2_ros
+from geometry_msgs.msg import Pose, PoseStamped, TransformStamped
+
+trans = ()
+rot = ()
 
 def getPose():
     
-    trans = ()
-    rot = ()
     try:
-        (trans, rot) = tl.lookupTransform("Camera", "tag1", rospy.Time(0))
-        msg = PoseStamped()
-        msg.position.x = trans[0]
-        msg.position.y = trans[1]
-        msg.position.z = trans[2]
-        msg.orientation.x = rot[0]
-        msg.orientation.y = rot[1]
-        msg.orientation.z = rot[2]
+        trans = buf.lookup_transform("tag1", "panda_link0", rospy.Time(0))
+        msg = Pose()
+        msg.position.x = trans.transform.translation.x
+        msg.position.y = trans.transform.translation.y
+        msg.position.z = trans.transform.translation.z
+        msg.orientation.x = trans.transform.rotation.x
+        msg.orientation.y = trans.transform.rotation.y
+        msg.orientation.z = trans.transform.rotation.z
         pub.publish(msg)
-        print("robot position published")
+
     except Exception as e:
+        print(e)
         pass
+
+def callback(msg):
+    t = TransformStamped()
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "panda_link0"
+    t.child_frame_id = "Camera"
+    t.transform.translation.x = msg.pose.position.x
+    t.transform.translation.y = msg.pose.position.y
+    t.transform.translation.z = msg.pose.position.z
+    t.transform.rotation.x = msg.pose.orientation.x
+    t.transform.rotation.y = msg.pose.orientation.y
+    t.transform.rotation.z = msg.pose.orientation.z
+    t.transform.rotation.w = msg.pose.orientation.w
+
+    br.sendTransform(t)
 
 
 if __name__ == "__main__":
     rospy.init_node('place_robot')
-    tl = tf.TransformListener()
+    buf = tf2_ros.Buffer()
+    tl = tf2_ros.TransformListener(buf)
+    br = tf2_ros.TransformBroadcaster()
+    sub = rospy.Subscriber('/panda_pose', PoseStamped, callback)
     pub = rospy.Publisher('/robot_pos', Pose, queue_size=1)
+    rate = rospy.Rate(50)
     while not rospy.is_shutdown():
         getPose()
+        rate.sleep()
